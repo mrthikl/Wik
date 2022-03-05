0f1f642228da0c8615c094e64bfe744ae0779c28
ThiNguyen <dragon_thi46@yahoo.com>
2022-03-05 22:28:58 +0700

    build sidebar-left

--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ $('.btn-menu.--open').on('click', function(e) {
 $('.btn-menu.--close').on('click', function(e) {
     $('.nav').removeClass('active')
 })
+
+// sidebar-left
+$('.sidebar-menu .has-drop a').on('click', function(e) {
+    e.preventDefault();
+})
+$('.sidebar-menu .has-drop').on('click', function(e) {
+    $(this).toggleClass('active')
+    if ($(this).hasClass('active')) {
+        $(this).css('max-height', $(this).prop('scrollHeight'))
+    } else {
+        $(this).css('max-height', '')
+    }
+})
